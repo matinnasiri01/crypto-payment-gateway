1df48d8113784d8f53a5ca67452679628df9fda9
Matin Nasiri <mnasirii829@gmail.com>
2026-06-26 20:26:28 +0330

    setup pgxpool as database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"crypto-payment-gateway/pkg/database"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Errorf("can`t find .env")
+	}
+}
 
 func main() {
-	fmt.Print("Hello from cpg")
+
+	_, err := database.NewPostgresDB(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Print("can`t connect to db")
+	}
 }
---
+++ pkg/database/postgres.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type PostgresDB struct {
+	*pgxpool.Pool
+}
+
+func NewPostgresDB(ctx context.Context, url string) (*PostgresDB, error) {
+	pool, err := pgxpool.New(ctx, url)
+	if err != nil {
+
+	}
+	defer pool.Close()
+
+	err = pool.Ping(ctx)
+	if err != nil {
+		pool.Close()
+		return nil, err
+	}
+
+	return &PostgresDB{pool}, nil
+}
